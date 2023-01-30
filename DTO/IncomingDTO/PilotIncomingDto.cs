afe6a9cb486d117e6ffb68e28380dc06cee85de4
Sadeesha-Dias <sddias19981230@gmail.com>
2023-01-30 19:32:43 +0530

    Created DTO  model for incoming request.

---
+++ DTO/IncomingDTO/PilotIncomingDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Swivel_AirLines.DTO.IncomingDTO
+{
+    public class PilotIncomingDto
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string PilotLisenceNumber { get; set; } = string.Empty;
+        public int FlyingHours { get; set; }
+    }
+}
